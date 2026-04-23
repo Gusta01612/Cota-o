d707c4a69061e7173df48faf1ae2c6065bff17a4
Gusta01612 <Gustafinanceiro@gmail.com>
2026-04-23 16:04:53 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,14 @@
+const CACHE_NAME = 'uno-v2'; // Mude de v1 para v2
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        fetch(event.request).catch(() => caches.match(event.request))
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
     );
 });
 
